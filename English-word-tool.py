6e7fffde4c54be4c3a46d41c8604a231ee3e1ea2
infstellar <98541813+infstellar@users.noreply.github.com>
2022-02-19 22:17:00 +0800

    Regular Update

--- English-word-tool.py
+++ English-word-tool.py
@@ -1,3 +1,5 @@
 import os
+print(os.path.abspath(__file__))
+
 os.chdir( os.path.dirname(os.path.abspath(__file__))+'\\mainwindow' )
 os.system('mainwindow.exe')
--- create publish version.bat
+++ create publish version.bat
@@ -1,3 +1,3 @@
-pyinstaller -w  mainwindow.py --distpath=../English-word-tool-publish-version
-pyinstaller -F English-word-tool.py --distpath=../English-word-tool-publish-version
+pyinstaller -w -F mainwindow.py --distpath=../English-word-tool-publish-version
+rem pyinstaller -F English-word-tool.py --distpath=../English-word-tool-publish-version
 cmd /k
